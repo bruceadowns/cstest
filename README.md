37631747b36c342edc1ed0cd35e70e6d7eac1c6b
Bruce A Downs <bruceadowns@gmail.com>
2016-02-24 21:05:40 -0700

    Cleaned out unused

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # cstest
-Test golang project to experiment with codeship
+Test golang project to experiment with codeship and travis-ci
 
 [![Codeship Status for bruceadowns/cstest](https://codeship.com/projects/78dac530-b961-0133-88e0-76946c995c7e/status?branch=master)](https://codeship.com/projects/135510)
 
--- cstest_test.go
+++ cstest_test.go
@@ -1,51 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"testing"
-)
-
-func count(s, r string) int {
-	if len(s) == 0 {
-		return 1
-	}
-
-	var res int
-	for i := 0; i < len(r); i++ {
-		if s[0] == r[i] {
-			res += count(s[1:], r[i+1:])
-		}
-	}
-
-	return res
-}
-
-func TestCount(t *testing.T) {
-	var tt = []struct {
-		s string
-		r string
-		e int
-	}{
-		{
-			s: "bar",
-			r: "barbar",
-			e: 4,
-		},
-		{
-			s: "abc",
-			r: "abcdefab",
-			e: 1,
-		},
-	}
-
-	for _, test := range tt {
-		c := count(test.s, test.r)
-		if c != test.e {
-			t.Fatalf("invalid count %d for %v", c, test)
-		}
-	}
-}
+import "testing"
 
 func TestMain(t *testing.T) {
-	fmt.Println("Hello Test")
+	t.Log("Hello Test")
 }
